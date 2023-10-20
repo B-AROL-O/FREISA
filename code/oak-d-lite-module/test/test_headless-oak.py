afe71e58f4ceb815d05dca6b86e6470c2f6a5ba3
davmacario <dav.macario@gmail.com>
2023-10-20 16:05:58 -0500

    Create runInferencePipeline function

--- code/oak-d-lite-module/test/test_headless-oak.py
+++ code/oak-d-lite-module/test/test_headless-oak.py
@@ -3,6 +3,8 @@ import cv2
 import blobconverter
 import numpy as np
 from datetime import datetime
+import os
+import sys
 import time
 
 
@@ -10,6 +12,131 @@ def frameNorm(frame, bbox):
     normVals = np.full(len(bbox), frame.shape[0])
     normVals[::2] = frame.shape[1]
     return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+
+def runInferencePipeline(
+    pipeline: dai.Pipeline,
+    max_time: float,
+    res_path: str,
+    pipeline_name: str = "Neural network",
+    disp: bool = False,
+    verb: bool = False,
+):
+    """
+    runInferencePipeline
+    ---
+    Run a specific inference pipeline on the OAK device.
+
+    The steps of the pipeline are the following:
+    - Initialize queues for communication with the camera
+    - Open the file (created if not existing)
+    - Start the capture
+    - Write on output file the detected objects only (with timestamp
+    and position in the field of view)
+
+    ### Input parameters
+    - pipeline: depthai.Pipeline object with initialized nodes; need
+    output streams (XLinkOut) "rgb" and "inference" to be initialized
+    - max_time: time duration for the use of the pipeline
+    - res_path: path of the output file
+    - pipeline_name: name of the pipeline (string) - used for printing
+    on stdout
+    - disp: flag to determine whether to reproduce video stream (not
+    the intended use case)
+    - verb: flag for printing runtime information
+    """
+    # If the output path does not exist, create it.
+    if not os.path.exists(res_path):
+        if verb:
+            print(f"Creating file {res_path}")
+        with open(res_path, "w") as f:
+            f.close()
+
+    t_start = time.time()
+    with dai.Device(pipeline) as device:
+        # Define queue for nn output - blocking=False will make only the most recent info available
+        queue_nn = device.getOutputQueue(name="inference", maxSize=1, blocking=False)
+        # Define queue for camera output - only if 'disp' flag is true
+        if disp:
+            queue_rgb = device.getOutputQueue("rgb", maxSize=1, blocking=False)
+
+        # NOTE: setting blocking=False makes inference quicker, as everytime the
+        # loop is repeated there will be no backlogged detections (old ones are
+        # discarded)
+
+        # Initialize placeholders for results:
+        frame = None  # Containing the output of the camera block
+        detections = []  # Containing the inference results
+
+        # Create a text file to store inference results
+        with open(res_path, "a") as output_file:
+            print("{} started".format(pipeline_name))
+            while time.time() - t_start < max_time:
+                # Timestamp
+                ts = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+
+                # Try to get an element from the output nn queue
+                in_nn = queue_nn.tryGet()
+
+                if disp:
+                    in_rgb = queue_rgb.tryGet()
+
+                    if in_rgb is not None:
+                        frame = in_rgb.getCvFrame()
+
+                flg_disp = False
+                if frame is not None and disp:
+                    flg_disp = True
+
+                # Get the detection results from the frame (if any)
+                if in_nn is not None:
+                    detections = in_nn.detections
+
+                    for detection in detections:
+                        # Get boundary coordinates of detected object
+                        x1, y1, x2, y2 = (
+                            int(detection.xmin),
+                            int(detection.ymin),
+                            int(detection.xmax),
+                            int(detection.ymax),
+                        )
+                        # Notice that the values of x and y are normalized to [0, 1]
+                        object_centroid = (0.5 * (x1 + x2), 0.5 * (y1 + y2))
+
+                        out_str = (
+                            "{}: Label: {} - {}, Confidence: {}; Position: {}\n".format(
+                                ts,
+                                detection.label,
+                                MN_CLASSES[detection.label],
+                                detection.confidence,
+                                object_centroid,
+                            )
+                        )
+
+                        if verb:
+                            print(out_str)
+
+                        output_file.write(out_str)
+
+                        if flg_disp:
+                            bbox = frameNorm(frame, (x1, y1, x2, y2))
+                            cv2.rectangle(
+                                frame,
+                                (bbox[0], bbox[1]),
+                                (bbox[2], bbox[3]),
+                                (255, 0, 0),
+                                2,
+                            )
+
+                    if flg_disp:
+                        print("here")
+                        cv2.imshow("preview", frame)
+                else:
+                    pass
+                    # output_file.write("{}\n".format(ts))
+
+                # wait for some time before next capture
+                time.sleep(0.5)
 
 
 VERB = True
@@ -120,10 +247,14 @@ YOLO_CLASSES = [
     "toothbrush",
 ]
 
-
 if __name__ == "__main__":
+    # Allow to pass output file name (relative path) as command line arg
+    if len(sys.argv) > 1:
+        output_path_rel = str(sys.argv[1])
+    else:
+        output_path_rel = "detection_results.txt"
     # Wipe the file with the outputs:
-    fname = "detection_results.txt"
+    fname = os.path.join(os.path.dirname(__file__), output_path_rel)
     with open(fname, "w") as f:
         f.close()
 
@@ -191,183 +322,9 @@ if __name__ == "__main__":
 
     ## Execution Loop (use each model for 20 seconds, then switch)
     while True:
-        # Create DepthAI device
-        t_mn_start = time.time()
-        with dai.Device(pipeline) as device:
-            # Define queue for nn output - blocking=False will make only the most recent info available
-            queue_nn = device.getOutputQueue(
-                name="inference", maxSize=1, blocking=False
-            )
-            # queue_nn = device.getOutputQueue(name="inference")
-
-            queue_rgb = device.getOutputQueue("rgb", maxSize=1, blocking=False)
-            # queue_rgb = device.getOutputQueue("rgb")
-
-            # NOTE: setting blocking=False makes inference quicker, as everytime the
-            # loop is repeated there will be no backlogged detections (old ones are
-            # discarded)
-
-            # Initialize placeholders for results:
-            frame = None  # Probably not used
-            detections = []
-
-            # Create a text file to store inference results
-            with open(fname, "a") as output_file:
-                print("MobileNet started")
-                while time.time() - t_mn_start < 20:
-                    # Timestamp
-                    ts = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-
-                    # Try to get an element from the output nn queue
-                    in_nn = queue_nn.tryGet()
-
-                    # frame = None
-                    if DISPLAY:
-                        in_rgb = queue_rgb.tryGet()
-
-                        if in_rgb is not None:
-                            frame = in_rgb.getCvFrame()
-
-                    flg_disp = False
-                    if frame is not None and DISPLAY:
-                        flg_disp = True
-
-                    # Get the detection results from the frame (if any)
-                    if in_nn is not None:
-                        detections = in_nn.detections
-
-                        for detection in detections:
-                            # Get boundary coordinates of detected object
-                            x1, y1, x2, y2 = (
-                                int(detection.xmin),
-                                int(detection.ymin),
-                                int(detection.xmax),
-                                int(detection.ymax),
-                            )
-                            # Notice that the values of x and y are normalized to [0, 1]
-                            object_centroid = (0.5 * (x1 + x2), 0.5 * (y1 + y2))
-
-                            out_str = "{}: Label: {} - {}, Confidence: {}; Position: {}\n".format(
-                                ts,
-                                detection.label,
-                                MN_CLASSES[detection.label],
-                                detection.confidence,
-                                object_centroid,
-                            )
-
-                            if VERB:
-                                print(out_str)
-
-                            output_file.write(out_str)
-
-                            if flg_disp:
-                                bbox = frameNorm(frame, (x1, y1, x2, y2))
-                                cv2.rectangle(
-                                    frame,
-                                    (bbox[0], bbox[1]),
-                                    (bbox[2], bbox[3]),
-                                    (255, 0, 0),
-                                    2,
-                                )
-
-                        if flg_disp:
-                            print("here")
-                            cv2.imshow("preview", frame)
-                    else:
-                        pass
-                        # output_file.write("{}\n".format(ts))
-
-                    # wait for some time before next capture
-                    time.sleep(0.5)
-
+        runInferencePipeline(pipeline, 20, fname, "MobileNet", disp=DISPLAY, verb=VERB)
         # Switch model
-        t_yolo_start = time.time()
-        with dai.Device(pipeline_1) as device:
-            # Define queue for nn output - blocking=False will make only the most recent info available
-            queue_nn = device.getOutputQueue(
-                name="inference", maxSize=1, blocking=False
-            )
-            # queue_nn = device.getOutputQueue(name="inference")
-
-            queue_rgb = device.getOutputQueue("rgb", maxSize=1, blocking=False)
-            # queue_rgb = device.getOutputQueue("rgb")
-
-            # NOTE: setting blocking=False makes inference quicker, as everytime the
-            # loop is repeated there will be no backlogged detections (old ones are
-            # discarded)
-
-            # Initialize placeholders for results:
-            frame = None  # Probably not used
-            detections = []
-
-            # Create a text file to store inference results
-            with open(fname, "a") as output_file:
-                print("YOLO started")
-                while time.time() - t_yolo_start < 20:
-                    # Timestamp
-                    ts = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-
-                    # Try to get an element from the output nn queue
-                    in_nn = queue_nn.tryGet()
-
-                    # frame = None
-                    if DISPLAY:
-                        in_rgb = queue_rgb.tryGet()
-
-                        if in_rgb is not None:
-                            frame = in_rgb.getCvFrame()
-
-                    flg_disp = False
-                    if frame is not None and DISPLAY:
-                        flg_disp = True
-
-                    # Get the detection results from the frame (if any)
-                    if in_nn is not None:
-                        detections = in_nn.detections
-
-                        for detection in detections:
-                            # Get boundary coordinates of detected object
-                            x1, y1, x2, y2 = (
-                                int(detection.xmin),
-                                int(detection.ymin),
-                                int(detection.xmax),
-                                int(detection.ymax),
-                            )
-                            # Notice that the values of x and y are normalized to [0, 1]
-                            object_centroid = (0.5 * (x1 + x2), 0.5 * (y1 + y2))
-
-                            out_str = "{}: Label: {} - {}, Confidence: {}; Position: {}\n".format(
-                                ts,
-                                detection.label,
-                                YOLO_CLASSES[detection.label],
-                                detection.confidence,
-                                object_centroid,
-                            )
-
-                            if VERB:
-                                print(out_str)
-
-                            output_file.write(out_str)
-
-                            if flg_disp:
-                                bbox = frameNorm(frame, (x1, y1, x2, y2))
-                                cv2.rectangle(
-                                    frame,
-                                    (bbox[0], bbox[1]),
-                                    (bbox[2], bbox[3]),
-                                    (255, 0, 0),
-                                    2,
-                                )
-
-                        if flg_disp:
-                            print("here")
-                            cv2.imshow("preview", frame)
-                    else:
-                        pass
-                        # output_file.write("{}\n".format(ts))
-
-                    # wait for some time before next capture
-                    time.sleep(0.5)
+        runInferencePipeline(pipeline_1, 20, fname, "YOLOv3", disp=DISPLAY, verb=VERB)
 
         if cv2.waitKey(1) == ord("q"):
             # This part of the code is not working!
