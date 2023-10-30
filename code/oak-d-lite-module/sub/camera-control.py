5ac4ac5e712851e9d3dc73b799d11e6b108e040f
davmacario <dav.macario@gmail.com>
2023-10-30 14:03:20 -0500

    Add camera-control script

---
+++ code/oak-d-lite-module/sub/camera-control.py
@@ -0,0 +1,31 @@
+import depthai as dai
+import cv2
+import blobconverter
+import numpy as np
+from datetime import datetime
+import os
+import sys
+import time
+
+"""
+Camera control library
+---
+This library contains functions used for controlling the operation of 
+the OAK-D lite camera.
+"""
+
+
+class VisionController:
+    def __init__(
+        self,
+        models_path: str,
+    ):
+        """
+        VisionController
+        ---
+
+        """
+
+
+if __name__ == "__main__":
+    pass
