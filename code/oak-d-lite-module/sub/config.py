aeadf703b82d262ce7f01fc5e3b6689ae0d0c7d2
davmacario <dav.macario@gmail.com>
2023-11-08 16:35:46 -0600

    Create config file for camera control

---
+++ code/oak-d-lite-module/sub/config.py
@@ -0,0 +1,12 @@
+"""
+Configuration file for camera_control module.
+
+This script contains the configuration parameters used to tune the models.
+"""
+
+# Runtime settings:
+DEBUG = True
+VERB = True
+
+# Confidence threshold for the models
+CONF_THRESH_NN = 0.5
