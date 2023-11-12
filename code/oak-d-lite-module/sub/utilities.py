713dcfeeb8ce3f8363e7ccd33488c0fec88530df
davmacario <dav.macario@gmail.com>
2023-11-12 04:01:17 -0600

    Add utilities script

---
+++ code/oak-d-lite-module/sub/utilities.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def frameNorm(frame, bbox):
+    normVals = np.full(len(bbox), frame.shape[0])
+    normVals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
