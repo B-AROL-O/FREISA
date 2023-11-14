20dec364201cf5ab07875fecf82d4e2636fbcad0
davmacario <dav.macario@gmail.com>
2023-11-13 20:01:27 -0600

    Add port binding

--- code/oak-d-lite-module/vision_container.sh
+++ code/oak-d-lite-module/vision_container.sh
@@ -5,5 +5,6 @@ docker run --rm \
     -v /dev/bus/usb:/dev/bus/usb \
     -v .:/oak-d-lite-module/ \
     --device-cgroup-rule='c 189:* rmw' \
+    -p 9090:9090 \
     depthai-test:latest \
     python3 /oak-d-lite-module/vision_webserver.py
