2857f69cb914755b7695d6ff2a9652132a734e37
Gianpaolo Macario <gmacario@gmail.com>
2025-08-27 16:58:12 +0000

    feat(test-freisa-faces): Make our doggo display all images in current dir

--- code/test-freisa-faces/python_api_display.py
+++ code/test-freisa-faces/python_api_display.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python
 
+from os import listdir
+from os.path import isfile, join
 import time
 
 from MangDang.mini_pupper.display import BehaviorState, Display
 
+print(f"INFO: {__file__}")
+
+mypath = "."
+
+# from os import walk
+# f = []
+# for (dirpath, dirnames, filenames) in walk(mypath):
+#     f.extend(filenames)
+#     break
+#
+# print(f"DEBUG: len={len(f)}, f={f}")
+
+# Retrieve the list of all "*.png" files in the current directory
+all_images = [
+    f for f in listdir(mypath)
+    if isfile(join(mypath, f))
+    and f[-4:] == ".png"]
+
+# print(f"DEBUG: len={len(all_images)}, all_images={all_images}")
+
+# Make our doggo display all_images in sequence
 disp = Display()
-
-# Hard code contents of mini_pupper_eyes_v2_fixed.zip
-
-disp.show_image("/home/ubuntu/test/eyes2_sorpresa.png")
-time.sleep(5)
-disp.show_image("/home/ubuntu/test/eyes2_pensiero_04.png")
-
-# TODO: Display in sequence all "*.png" files in the current directory
+k = 0
+for f in sorted(all_images):
+    k += 1
+    print(f"DEBUG: Displaying file #{k}: {f}")
+    disp.show_image(f)
+    time.sleep(5)
 
 # disp.show_image('/var/lib/mini_pupper_bsp/test.png')
-
 # time.sleep(5)
 # disp.show_state(BehaviorState.REST)
 # time.sleep(5)
