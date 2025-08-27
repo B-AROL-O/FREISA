af52e48295d7bb984c86561c80f011a60d36b85c
Gianpaolo Macario <gmacario@gmail.com>
2025-08-27 15:52:25 +0000

    chore(test-freisa-faces): fix Super-Linter errors

--- code/test-freisa-faces/python_api_display.py
+++ code/test-freisa-faces/python_api_display.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
-from MangDang.mini_pupper.display import Display, BehaviorState
+
 import time
+
+from MangDang.mini_pupper.display import BehaviorState, Display
 
 disp = Display()
 
-disp.show_image('/var/lib/mini_pupper_bsp/test.png')
+# Hard code contents of mini_pupper_eyes_v2_fixed.zip
+
+disp.show_image("/home/ubuntu/test/eyes2_sorpresa.png")
 time.sleep(5)
-disp.show_state(BehaviorState.REST)
-time.sleep(5)
-disp.show_state(BehaviorState.TROT)
-time.sleep(5)
+disp.show_image("/home/ubuntu/test/eyes2_pensiero_04.png")
+
+# disp.show_image('/var/lib/mini_pupper_bsp/test.png')
+# time.sleep(5)
+
+# disp.show_state(BehaviorState.REST)
+# time.sleep(5)
+# disp.show_state(BehaviorState.TROT)
+# time.sleep(5)
 disp.show_state(BehaviorState.LOWBATTERY)
 time.sleep(5)
 disp.show_ip()
+
+# EOF
