54137912354bbfe8a27bf4987e37b1dfbf38d234
Gianpaolo Macario <gmacario@gmail.com>
2025-09-03 17:39:14 +0000

    feat(dogface-server): change API from /face/available to /status

--- code/dogface-server/dogface.py
+++ code/dogface-server/dogface.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 # <https://github.com/mangdangroboticsclub/apps-md-robots/tree/main/facial-expression-app>
 
 # TODO: Attempt to import MangDang. Fallback to simulation on error
+# from MangDang.mini_pupper.display import Display, BehaviorState
 
 app = Flask(__name__)
 
@@ -12,11 +13,17 @@ app = Flask(__name__)
 available_faces = ["alice", "bob", "charlie"]
 current_face = None
 
+available_sounds = ["bark", "woof", "sign"]
+
 # TODO: Should we need a mapping between sentiment and filename?
 
-@app.route("/face/available", methods=["GET"])
+@app.route("/status", methods=["GET"])
 def list_known_faces():
-    return jsonify({"available_faces": available_faces})
+    return jsonify(
+        {
+            "available_faces": available_faces,
+            "available_sounds": available_sounds
+        })
 
 @app.route("/face/set", methods=["POST"])
 def set_face():
@@ -31,9 +38,16 @@ def set_face():
         return jsonify({"error": f"Face '{new_face_id}' not recognized"}), 404
     
     # TODO: Load the picture using MangDang.display
-    
+    # disp = Display()
+    # disp.show_image('/var/lib/mini_pupper_bsp/test.png')
+
     current_face = new_face_id
     return jsonify({"message": f"Current face set to {current_face}"}), 200
+
+# @app.route("/play/sound", methods=["POST"])
+# def play_sound():
+#     See <https://github.com/mangdangroboticsclub/mini_pupper_2_bsp/blob/mini_pupper_2pro_bsp/demos/audio_test.py>
+#     ...
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
