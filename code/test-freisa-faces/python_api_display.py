eb2b5286a533e7a6407e882409f08e9a8400c32b
Gianpaolo Macario <gmacario@gmail.com>
2025-09-04 04:20:53 +0000

    chore(test-freisa-faces): source code cleanup

--- code/test-freisa-faces/python_api_display.py
+++ code/test-freisa-faces/python_api_display.py
@@ -4,13 +4,12 @@ import time
 from os import listdir
 from os.path import isfile, join
 
-# from MangDang.mini_pupper.display import BehaviorState, Display
-
 print(f"INFO: {__file__}")
 
 on_freisa = False
 
 try:
+    # Try importing package MangDang
     from MangDang.mini_pupper.display import BehaviorState, Display
     on_freisa = True
     print("DEBUG: Running on FREISA")
@@ -20,14 +19,6 @@ except ImportError:
 basedir = "."
 if on_freisa:
     basedir = "/home/ubuntu/FREISA/assets/faces"
-
-# from os import walk
-# f = []
-# for (dirpath, dirnames, filenames) in walk(mypath):
-#     f.extend(filenames)
-#     break
-#
-# print(f"DEBUG: len={len(f)}, f={f}")
 
 # Retrieve the list of all "*.png" files in the current directory
 all_images = [
@@ -46,13 +37,6 @@ for f in sorted(all_images):
     if on_freisa:
         disp.show_image(f)
     time.sleep(5)
-
-# disp.show_image('/var/lib/mini_pupper_bsp/test.png')
-# time.sleep(5)
-# disp.show_state(BehaviorState.REST)
-# time.sleep(5)
-# disp.show_state(BehaviorState.TROT)
-# time.sleep(5)
 
 if on_freisa:
     disp.show_state(BehaviorState.LOWBATTERY)
